b788341bba0d0d619fbdbdbdd282150ad2ca4dfc
KimiNako <katran@etud.insa-toulouse.fr>
2019-11-21 23:06:31 +0100

    Cleaning code for TP3

--- TP3_SVM/TP3_prog1.py
+++ TP3_SVM/TP3_prog1.py
@@ -3,73 +3,36 @@ from sklearn import model_selection
 from sklearn.datasets import fetch_openml
 from sklearn.svm import SVC
 
+import matplotlib.pyplot as plt
 import numpy as np
 import time
 
 
 mnist = fetch_openml('mnist_784')
 
+# --------------------------------------------------------------------
+# Test avec un training à 70% pour l’apprentissage et 30% pour les tests
+# --------------------------------------------------------------------
 
-percent = 0.7
-index_vect = np.random.randint(70000, size=5000)
-data = mnist.data[index_vect]
-target = mnist.target[index_vect]
+xtrain, xtest, ytrain, ytest = model_selection.train_test_split(minst.data, minst.target, train_size=0.7)
 
-xtrain, xtest, ytrain, ytest = model_selection.train_test_split(data, target, train_size=percent)
+# ----------------- Variation de la fonction noyau -----------------
+#TODO check for precomputed vers
+for noyau in ['linear','poly','rbf','sigmoid','precomputed']:
+	classifier = SVC(gamma='auto', kernel=noyau) 
+	classifier.fit(xtrain, ytrain)
+	predicted = classifier.predict(xtest)
+	score = classifier.score(xtest, ytest)
+	print("Score : " + str(score))
 
-
-classifier = SVC(gamma='auto') 
-classifier.fit(xtrain, ytrain)
-predicted = classifier.predict(xtest)
-score = classifier.score(xtest, ytest)
-print("Score : " + str(score))
-
-
-
-clsvm = SVC(kernel='linear')
-clsvm.fit(xtrain, ytrain)
-predicted = clsvm.predict(xtest)
-score = clsvm.score(xtest, ytest)
-print("Score : " + str(score))
-
-
-clsvm = SVC(kernel='poly')
-clsvm.fit(xtrain, ytrain)
-predicted = clsvm.predict(xtest)
-score = clsvm.score(xtest, ytest)
-print("Score : " + str(score))
-
-clsvm = SVC(kernel='rbf')
-clsvm.fit(xtrain, ytrain)
-predicted = clsvm.predict(xtest)
-score = clsvm.score(xtest, ytest)
-print("Score : " + str(score))
-
-
-clsvm = SVC(kernel='sigmoid')
-clsvm.fit(xtrain, ytrain)
-predicted = clsvm.predict(xtest)
-score = clsvm.score(xtest, ytest)
-print("Score : " + str(score))
-
-
-#TODO
-clsvm = SVC(kernel='precomputed')
-clsvm.fit(xtrain, ytrain)
-predicted = clsvm.predict(xtest)
-score = clsvm.score(xtest, ytest)
-print("Score : " + str(score))
-
-
+# ------ Variation du paramètre de tolerance aux erreurs C ---------
 erreur = []
 for tolerancce in [0.1,0.3,0.5,0.7,1.0]:
     clsvm = SVC(gamma='auto', kernel='poly' ,C=tolerancce)
     clsvm.fit(xtrain, ytrain)
     predicted = clsvm.predict(xtest) 
     erreur.append(metrics.zero_one_loss(ytest, predicted))
-
 print(erreur)
-import matplotlib.pyplot as plt
 plt.plot([0.1,0.3,0.5,0.7,1.0], erreur)
 plt.xlabel("Valeur de C")
 plt.ylabel("Erreur de classification")
@@ -81,7 +44,8 @@ plt.show()
 cm = confusion_matrix(ytest, Y_pred)
 print(cm)
 
-
+# -------------------------------- Variation de la fonction noyau et de C --------------------------------
+# Analyse du temps d'apprentissage, de la précision, du rappel, de l'erreur et de la matrice de confusion 
 
 # il faut prendre le temps en cpu et non en temps "normal" (ne pas tenir compte de cette remarque pour TP1): time.clock()
 for noyau in ['linear','poly','rbf','sigmoid','precomputed']:
@@ -99,33 +63,25 @@ for noyau in ['linear','poly','rbf','sigmoid','precomputed']:
         erreur.append(metrics.zero_one_loss(ytest, predicted))
         precision.append(metrics.precision_score(ytest, predicted,average='micro'))
         rappel.append(metrics.recall_score(ytest, predicted,average='micro'))
-    print("Noyau : "+ noyau + " / Erreur : " + str(erreur))
-    plt.plot([0.1,0.3,0.5,0.7,1.0], erreur)
-    plt.xlabel("Valeur de C")
-    plt.ylabel("Erreur de classification")
-    plt.title("Noyau : "+ noyau + " / Erreur en fonction de C")
-    plt.show()
-    
-    print("Noyau : "+ noyau + " / Temps : " + str(temps))
-    plt.plot([0.1,0.3,0.5,0.7,1.0], temps)
-    plt.xlabel("Valeur de C")
-    plt.ylabel("Temps d'apprentissage")
-    plt.title("Noyau : "+ noyau + " / Temps en fonction de C")
-    plt.show()
-    
-    print("Noyau : "+ noyau + " / Précision : " + str(precision))
-    plt.plot([0.1,0.3,0.5,0.7,1.0], precision)
-    plt.xlabel("Valeur de C")
-    plt.ylabel("Precision")
-    plt.title("Noyau : "+ noyau + " / Precision en fonction de C")
-    plt.show()
-    
-    print("Noyau : "+ noyau + " / Rappel : " + str(rappel))
-    plt.plot([0.1,0.3,0.5,0.7,1.0], rappel)
-    plt.xlabel("Valeur de C")
-    plt.ylabel("Rappel")
-    plt.title("Noyau : "+ noyau + " / Rappel en fonction de C")
-    plt.show()
+    for type_analyse in ["Erreur", "Temps", "Précision", "Rappel"]:
+			if(type_analyse == "Erreur"):
+				content = erreur
+				ylabel = "Erreur de classification"
+			elif(type_analyse == "Temps"):
+				content = temps
+				ylabel = "Temps d'apprentissage"
+			elif(type_analyse == "Precision"):
+				content = precision
+				ylabel = "Precision"
+			else:
+				content = rappel
+				ylabel = "Rappel"
+			print("Noyau : "+ noyau + " / "+ type_analyse +" : " + str(content))
+			plt.plot([0.1,0.3,0.5,0.7,1.0], erreur)
+			plt.xlabel("Valeur de C")
+			plt.ylabel(ylabel)
+			plt.title("Noyau : "+ noyau + " / "+ type_analyse +" en fonction de C")
+			plt.show()
 
 
 
